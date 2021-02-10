ada545c999656c0cff140c51f39d936953b8bd8c
NilsOn <johnynar@gmail.com>
2021-02-10 17:52:25 +0300

    nav

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
+const { sessionVariables } = require("../middleware/session");
 
-router.route('/')
-  .get(sessionVariables, (req,res)=>{
-    // if(req.session.user){
-        res.render('main')
-    // }else{
-        // res.redirect('/login')
-    // }
+router.route("/").get(sessionVariables, (req, res) => {
+  // if(req.session.user){
+  res.render("main");
+  // }else{
+  // res.redirect('/login')
+  // }
 });
 
 module.exports = router;
