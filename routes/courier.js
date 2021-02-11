4186a9bd532edadbeaee25ab71b5c2c71ddb2bc0
NilsOn <johnynar@gmail.com>
2021-02-11 19:51:48 +0300

    courier rout v1

--- routes/courier.js
+++ routes/courier.js
@@ -2,34 +2,38 @@ const router = require("express").Router();
 // const { sessionChecker } = require("../middleware/session");
 const { sessionVariables } = require("../middleware/session");
 const User = require("../models/user");
-
-// router
-//   .route("/")
-// .get(sessionChecker, (req, res) => {
-// res.send('Hello, it\'s login page!');
-// res.render("login");
-// })
+const Order = require("../models/order");
 
 router.route("/").get(sessionVariables, (req, res) => {
-  // if(req.session.user){
   res.render("main");
-  // }else{
-  // res.redirect('/login')
-  // }
 });
-// .post(async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
-//   // console.log(user);
+router
+  .route("/new")
+  .get((req, res) => {
+    res.render("courier");
+  })
+  .post(async (req, res) => {
+    let { components, pricePrimary } = req.body;
 
-//   if (user && (await bcrypt.compare(password, user.password))) {
-//     req.session.email = user.email;
-//     // console.log("OK");
+    const newOrder = new Order({
+      components,
+      pricePrimary,
+      priceDiscount: Math.floor(pricePrimary * 0.5),
+    });
+    // console.log(newOrder);
+    await newOrder.save();
 
-//     res.redirect("/");
-//   } else {
-//     res.render("login", { mes: "Incorrect login/password" });
-//   }
-// });
+    console.log(typeof newOrder.statusBooked);
+
+    res.status(200).json(
+      // components,
+      // statusBooked,
+      // statusSold,
+      // pricePrimary,
+      // priceDiscount,
+      // adress,
+      newOrder
+    );
+  });
 
 module.exports = router;
