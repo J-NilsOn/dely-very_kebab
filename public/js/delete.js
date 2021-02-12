ef97ffa27bec0ce535dd3b37ced11e1ac4e9623d
NilsOn <johnynar@gmail.com>
2021-02-12 12:37:47 +0300

    delete btn

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,6 +1,6 @@
-function addDeleteListener(butons) {
-  if (butons.length) {
-    delBtns.forEach((btn) => {
+function addDeleteListener(button) {
+  if (button.length) {
+    button.forEach((btn) => {
       btn.addEventListener("click", async ({ target }) => {
         const { value } = target;
         const div = target.parentElement;
@@ -20,24 +20,26 @@ function addDeleteListener(butons) {
         }
       });
     });
-  } else {
-    butons.addEventListener("click", async ({ target }) => {
-      const { value } = target;
-      const li = target.parentElement;
-
-      const response = await fetch("/user", {
-        method: "DELETE",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ id: value }),
-      });
-      const result = await response.json();
-      if (result.success) {
-        li.remove();
-      } else {
-        alert(result.message);
-      }
-    });
   }
+
+  // else {
+  //   button.addEventListener("click", async ({ target }) => {
+  //     const { value } = target;
+  //     const div = target.parentElement;
+
+  //     const response = await fetch("/courier", {
+  //       method: "DELETE",
+  //       headers: {
+  //         "Content-type": "application/json",
+  //       },
+  //       body: JSON.stringify({ id: value }),
+  //     });
+  //     const result = await response.json();
+  //     if (result.success) {
+  //       div.remove();
+  //     } else {
+  //       alert(result.message);
+  //     }
+  //   });
+  // }
 }
